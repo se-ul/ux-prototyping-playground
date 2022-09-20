38de8aac4976cbc6518883904908ef2efa57de7f
jihye-sss <113980028+jihye-sss@users.noreply.github.com>
2022-09-20 20:13:09 +0900

    dkdkkd

--- src/projects/jihye/Jihye.tsx
+++ src/projects/jihye/Jihye.tsx
@@ -1,6 +1,7 @@
 import { motion } from "framer-motion";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { useMouse } from "react-use";
+import { Card } from "./Card";
 import "./Jihye.css";
 
 export const Jihye: React.FC = () => {
@@ -8,45 +9,50 @@ export const Jihye: React.FC = () => {
   const ref = useRef(null);
   const { elX, elY } = useMouse(ref);
 
+  // data
+  const [selectedCard, setSelectedCard] = useState(-1);
+
   // ui
   return (
-    <div ref={ref} className="jihye-container">
-      <motion.div className="jihye-background-filter">jihye</motion.div>
+    <div
+      ref={ref}
+      className="jihye-container"
+      // onClick={() => setSelectedCard(-1)}
+    >
       <motion.div
-        className="jihye-dice"
-        // animate={{
-        //   x: elX,
-        //   y: elY,
-        // }}
-        initial={{
-          background:
-            "linear-gradient(20deg, rgba(0,111,255,1) 0%, rgba(0,255,218,1) 100%)",
-          clipPath: `polygon(
-            50% 0%,
-            90% 20%,
-            100% 60%,
-            75% 100%,
-            25% 100%,
-            0% 60%,
-            10% 20%
-          )`,
+        className="jihye-card-container jihye-card-container-1"
+        animate={{
+          rotateY: -10,
+          rotateX: -10,
+          rotateZ: selectedCard === 0 ? -40 : -10,
+          x: selectedCard === 0 ? -200 : -30,
+          y: selectedCard === 0 ? -300 : 0,
         }}
-        whileHover={{
-          scale: 1.2,
-          background:
-            "linear-gradient(120deg, rgba(0,111,255,1) 0%, rgba(0,255,218,1) 100%)",
-          clipPath: "circle(50% at 50% 50%)",
-        }}
-        transition={{
-          type: "spring",
-          stiffness: 100,
-          damping: 15,
-        }}
+        onClick={() => setSelectedCard(0)}
       >
-        <div className="jihye-number"></div>
-        <div className="jihye-emoji-container">
-          <div className="jihye-emoji">🦋</div>
-        </div>
+        <Card></Card>
+      </motion.div>
+      <motion.div
+        className="jihye-card-container jihye-card-container-2"
+        animate={{
+          translateY: -40,
+          x: -10,
+          y: selectedCard === 1 ? -300 : 0,
+        }}
+        onClick={() => setSelectedCard(1)}
+      >
+        <Card></Card>
+      </motion.div>
+      <motion.div
+        className="jihye-card-container jihye-card-container-3"
+        animate={{
+          rotateZ: selectedCard === 2 ? 40 : 10,
+          x: selectedCard === 2 ? 200 : 30,
+          y: selectedCard === 2 ? -300 : 0,
+        }}
+        onClick={() => setSelectedCard(2)}
+      >
+        <Card></Card>
       </motion.div>
     </div>
   );
