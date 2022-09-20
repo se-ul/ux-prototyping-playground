b7ee3335a614266e4ded3c874919de0bb1c5ffb4
jihye-sss <113980028+jihye-sss@users.noreply.github.com>
2022-09-20 19:31:21 +0900

    jihye 하하

--- src/projects/jihye/Jihye.tsx
+++ src/projects/jihye/Jihye.tsx
@@ -11,15 +11,31 @@ export const Jihye: React.FC = () => {
   // ui
   return (
     <div ref={ref} className="jihye-container">
+      <motion.div className="jihye-background-filter">jihye</motion.div>
       <motion.div
         className="jihye-dice"
-        animate={{
-          x: elX,
-          y: elY,
+        // animate={{
+        //   x: elX,
+        //   y: elY,
+        // }}
+        initial={{
+          background:
+            "linear-gradient(20deg, rgba(0,111,255,1) 0%, rgba(0,255,218,1) 100%)",
+          clipPath: `polygon(
+            50% 0%,
+            90% 20%,
+            100% 60%,
+            75% 100%,
+            25% 100%,
+            0% 60%,
+            10% 20%
+          )`,
         }}
         whileHover={{
           scale: 1.2,
-          backgroundColor: "blue",
+          background:
+            "linear-gradient(120deg, rgba(0,111,255,1) 0%, rgba(0,255,218,1) 100%)",
+          clipPath: "circle(50% at 50% 50%)",
         }}
         transition={{
           type: "spring",
@@ -27,9 +43,9 @@ export const Jihye: React.FC = () => {
           damping: 15,
         }}
       >
-        <div className="jihye-number">하이</div>
+        <div className="jihye-number"></div>
         <div className="jihye-emoji-container">
-          <div className="jihye-emoji">🟢</div>
+          <div className="jihye-emoji">🦋</div>
         </div>
       </motion.div>
     </div>
