bdbb0e4b5fe6c8e5a5779824945dd5b919d112ce
hyonihyonihyoni <hyoni0120@gmail.com>
2022-09-20 20:12:28 +0900

    hyoni 2일차 완성

--- src/projects/hyoni/Hyoni.tsx
+++ src/projects/hyoni/Hyoni.tsx
@@ -1,6 +1,7 @@
 import { motion } from "framer-motion";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { useMouse } from "react-use";
+import { Card } from "./Card";
 import "./Hyoni.css";
 
 export const Hyoni: React.FC = () => {
@@ -10,57 +11,50 @@ export const Hyoni: React.FC = () => {
   // input
 
   // data
+  const [slectiedCard, setSelectedCard] = useState(-1);
 
   // ui
   return (
-    <div ref={ref} className="hyoni-playground-container">
+    <div
+      ref={ref}
+      className="hyoni-playground-container"
+      // onClick={() => setSelectedCard(-1)}
+    >
       <motion.div
-        className="hyoni-background-filter"
-        initial={{
-          clipPath: `polygon(
-          50% 0%, 83% 12%, 100% 43%, 94% 78%, 68% 100%, 32% 100%, 6% 78%, 0% 43%, 17% 12%
-        )`,
-        }}
-        whileHover={{
-          clipPath: `polygon(
-          20% 0%, 80% 0%, 100% 20%, 100% 80%, 80% 100%, 20% 100%, 0% 80%, 0% 20%
-        )`,
-        }}
-      ></motion.div>
-      {/* {elX} {elY} */}
-      <motion.div
-        className="hyoni-dice"
+        className="hyoni-card-container hyoni-card-container-1"
         animate={{
-          x: elX,
-          y: elY,
+          x: -80,
+          y: slectiedCard === 0 ? -300 : 0,
+          scale: slectiedCard === 0 ? 1.5 : 1,
+          rotateZ: -20,
         }}
-        // whileHover={{
-        //   scale: 2,
-        //   skewX: 10,
-        //   background: "black",
-        // }}
-        initial={{
-          background:
-            "linear-gradient(90deg, rgba(179,255,172,1) 0%, rgba(184,237,238,1) 100%)",
-        }}
-        whileHover={{
-          background:
-            "linear-gradient(90deg, rgba(179,255,172,1) 0%, rgba(184,237,238,1) 100%)",
-        }}
-        whileTap={{
-          scale: 0.9,
-        }}
-        transition={{
-          type: "spring",
-          stiffness: 400,
-          damping: 10,
-          mass: 1,
+        onClick={() => {
+          setSelectedCard(0);
         }}
       >
-        <div className="hyoni-number">ball</div>
-        <div className="hyoni-emoji-container">
-          <div className="hyoni-emoji">🎾</div>
-        </div>
+        <Card></Card>
+      </motion.div>
+      <motion.div
+        className="hyoni-card-container hyoni-card-container-2"
+        animate={{
+          y: slectiedCard === 1 ? -300 : -30,
+          scale: slectiedCard === 1 ? 1.5 : 1,
+        }}
+        onClick={() => setSelectedCard(1)}
+      >
+        <Card></Card>
+      </motion.div>
+      <motion.div
+        className="hyoni-card-container hyoni-card-container-3"
+        animate={{
+          x: 80,
+          y: slectiedCard === 2 ? -300 : 0,
+          scale: slectiedCard === 2 ? 1.5 : 1,
+          rotateZ: 20,
+        }}
+        onClick={() => setSelectedCard(2)}
+      >
+        <Card></Card>
       </motion.div>
     </div>
   );
