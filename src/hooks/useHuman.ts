1266a95ee6dd23a07dd6736c1d06e9a60484088e
se-ul <o@o0oo0o.ooo>
2022-12-20 20:41:42 +0900

    가랏

---
+++ src/hooks/useHuman.ts
@@ -0,0 +1,42 @@
+import Human, { Config, FaceResult, Result } from "@vladmandic/human";
+import { RefObject, useEffect, useRef, useState } from "react";
+import Webcam from "react-webcam";
+
+const humanConfig: Partial<Config> = {
+  modelBasePath: "/models",
+  face: {
+    enabled: true,
+    detector: { rotation: true },
+  },
+};
+
+const human = new Human(humanConfig);
+
+export function useHuman(webcamRef: RefObject<Webcam>) {
+  const rafRef = useRef<number>();
+  const [faces, setFaces] = useState<FaceResult[]>([]);
+
+  useEffect(() => {
+    webcamRef.current.video.width = webcamRef.current.video.videoWidth;
+    webcamRef.current.video.height = webcamRef.current.video.videoHeight;
+
+    const detect = async () => {
+      const result = await detectFace(webcamRef.current);
+      setFaces(result.face);
+      rafRef.current = requestAnimationFrame(detect);
+    };
+
+    detect();
+
+    return () => cancelAnimationFrame(rafRef.current);
+  }, [webcamRef]);
+
+  return faces;
+}
+
+async function detectFace(webcam: Webcam): Promise<Result> {
+  const video = webcam.video;
+  video.width = webcam.video.videoWidth;
+  video.height = webcam.video.videoHeight;
+  return await human.detect(video);
+}
